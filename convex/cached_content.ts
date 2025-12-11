bbaa2ea86ed2dfb8f08addd778b1e89450ba5d76
LukeTandjung <lukelucus123@gmail.com>
2025-12-11 07:45:36 +0000

    Added cached_content backend api

---
+++ convex/cached_content.ts
@@ -0,0 +1,26 @@
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const get_cached_articles = query({
+  args: { user_id: v.id("users") },
+  handler: async (ctx, args) => {
+    const articles = await ctx.db
+      .query("cached_content")
+      .filter((q) => q.eq(q.field("user_id"), args.user_id))
+      .order("desc")
+      .collect();
+    return articles;
+  },
+});
+
+export const get_cached_articles_by_feed = query({
+  args: { rss_feed_id: v.id("rss_feed") },
+  handler: async (ctx, args) => {
+    const articles = await ctx.db
+      .query("cached_content")
+      .withIndex("by_rss_feed_id", (q) => q.eq("rss_feed_id", args.rss_feed_id))
+      .order("desc")
+      .collect();
+    return articles;
+  },
+});
