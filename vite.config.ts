7e4ec0c3090178fb05c4cd79f4bc15c4454314f6
LukeTandjung <lukelucus123@gmail.com>
2025-12-21 02:14:16 +0000

    Installed React compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,27 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import babel from "vite-plugin-babel";
+
+const ReactCompilerConfig = {
+  /* ... */
+};
 
 export default defineConfig({
   base: "/",
   server: {
     port: 3000,
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
