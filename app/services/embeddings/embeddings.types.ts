cd661bc6704079afaded94d4cdd2b318a01d77e8
LukeTandjung <lukelucus123@gmail.com>
2026-01-28 19:54:58 +0000

    Refactored Embeddings to use Effect Agent SDK API

--- app/services/embeddings/embeddings.types.ts
+++
@@ -1,4 +0,0 @@
-export interface EmbeddingResult {
-  embedding: Array<number>;
-  tokens: number;
-}
--- app/services/embeddings/index.ts
+++ app/services/embeddings/index.ts
@@ -1,4 +1,1 @@
-export { EmbeddingService, DedalusClientService } from "./embeddings.service";
-export { EmbeddingServiceLive, DedalusClientServiceLive } from "./embeddings.effect";
-export { EmbeddingError } from "./embeddings.errors";
-export type { EmbeddingResult } from "./embeddings.types";
+export { TextEmbedding } from "services/agents/agents.effect";
